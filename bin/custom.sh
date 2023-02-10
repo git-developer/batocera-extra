aa44bf7543b626d04ece8d2c63ad4fb64e48541e
Christian <git-developer@users.noreply.github.com>
2023-02-10 05:57:33 +0100

    refactor: separate features, patches and scripts

--- bin/custom.sh
+++ bin/custom.sh
@@ -6,11 +6,11 @@ case "${1-}" in
   start)
            /userdata/extra/joycond-cemuhook/install-and-start
            /userdata/extra/ds4drv-cemuhook/install-and-start
-           /userdata/extra/bin/patch-citra-generator-for-language
-           /userdata/extra/bin/patch-citra-generator-for-cemuhook
-           /userdata/extra/bin/patch-cemu-generator-for-squashfs
-           /userdata/extra/bin/patch-cemu-generator-for-cemuhook
-           /userdata/extra/bin/patch-batocera-info-for-pro-controller
+           /userdata/extra/patches/patch-citra-generator-for-language
+           /userdata/extra/patches/patch-citra-generator-for-cemuhook
+           /userdata/extra/patches/patch-cemu-generator-for-squashfs
+           /userdata/extra/patches/patch-cemu-generator-for-cemuhook
+           /userdata/extra/patches/patch-batocera-info-for-pro-controller
            /userdata/extra/remote-touchpad/start
            ;;
   stop)    /userdata/extra/remote-touchpad/stop
