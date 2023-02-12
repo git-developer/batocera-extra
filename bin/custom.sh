b1c872afb7508f776e1edf73aa40cfebebdbc0dc
Christian <git-developer@users.noreply.github.com>
2023-02-12 19:27:39 +0100

    refactor: mark dbus-python as installed

--- bin/custom.sh
+++ bin/custom.sh
@@ -4,13 +4,14 @@ echo "$(date -Iseconds)" "${0}" "${@}" >&2
 
 case "${1-}" in
   start)
-           /userdata/extra/joycond-cemuhook/install-and-start
-           /userdata/extra/ds4drv-cemuhook/install-and-start
            /userdata/extra/patches/patch-citra-generator-for-language
            /userdata/extra/patches/patch-citra-generator-for-cemuhook
            /userdata/extra/patches/patch-cemu-generator-for-squashfs
            /userdata/extra/patches/patch-cemu-generator-for-cemuhook
            /userdata/extra/patches/patch-batocera-info-for-pro-controller
+           /userdata/extra/patches/mark-dbus-python-as-installed
+           /userdata/extra/joycond-cemuhook/install-and-start
+           /userdata/extra/ds4drv-cemuhook/install-and-start
            /userdata/extra/remote-touchpad/start
            ;;
   stop)    /userdata/extra/remote-touchpad/stop
